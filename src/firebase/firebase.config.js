b2bea074803795f128687d5b4286428f731b2211
Anisur Rahman <anisur.ctg0917@gmail.com>
2025-05-06 01:47:00 +0600

    env set

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFElRqbTOwgqCFnm_GXHXRA7A3cvgj088",
-  authDomain: "mod53asgmnt.firebaseapp.com",
-  projectId: "mod53asgmnt",
-  storageBucket: "mod53asgmnt.firebasestorage.app",
-  messagingSenderId: "408487869731",
-  appId: "1:408487869731:web:3e840047eebcc98381a096"
+  apiKey:            import.meta.env.VITE_apiKey,
+  authDomain:        import.meta.env.VITE_authDomain,
+  projectId:         import.meta.env.VITE_projectId,
+  storageBucket:     import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId:             import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
